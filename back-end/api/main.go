8ac1825a776addfe547fbb89f7903214ddaeb9c5
Jordan Sheehan <jordansheehan26@gmail.com>
2023-01-30 13:25:27 -0500

    Created basic structure for Go router.

---
+++ back-end/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+func InitializeRouter() {
+	r := mux.NewRouter();
+
+	// Subrouter for handling all requests made to API URL
+	s := r.PathPrefix("/api").Subrouter();
+
+	// Handle CORS options
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowCredentials: true,
+		AllowedMethods: []string{"GET", "POST", "PUT", "HEAD", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Accept-Language", "Content-Type", "Content-Language", "Origin"},
+	})
+	handler := c.Handler(r)
+
+	log.Println("Listening on port 9000...");
+	log.Fatal(http.ListenAndServe(":9000", handler))
+}
+
+func main() {
+	InitializeRouter()
+}
