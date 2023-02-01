5bc5f0cf16d77a69728761a8a41ff66c1d81fbd2
dylant313 <dylant313@yahoo.com>
2023-02-01 16:59:55 -0500

    Improved login screen design

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LoginComponent },];
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: LoginComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
