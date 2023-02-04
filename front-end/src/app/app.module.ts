b59c3b714bab37e3a85526430936f7bfce5e4d53
sarahp19 <spatel18@ufl.edu>
2023-02-04 15:01:43 -0500

    Added basic sign up screen design

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
