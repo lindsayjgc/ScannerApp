2b62aa1928a9ab9ace606a3a8baa8692b7f91d0a
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-02-19 17:13:15 -0500

    create test for AddAllergy function

--- back-end/api/info_test.go
+++ back-end/api/info_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,7 +26,7 @@ func TestUserInfo(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Create a new ResponseRecorder to record the response
+	// Create a new ResponseRecorder
 	rr := httptest.NewRecorder()
 	var allInfo AllUserInfo
 
@@ -60,4 +61,52 @@ func TestUserInfo(t *testing.T) {
 	if allInfo.Allergies != "NONE" {
 		t.Error("Allergies not as expected")
 	}
+}
+
+func TestAddAllergy(t *testing.T) {
+	// Initialize router and connect to DB for this test instance
+	InitialUserMigration()
+	InitialInfoMigration()
+	InitializeRouter()
+
+	info := Info{
+		Email:     "js@gmail.com",
+		Allergies: "testAllergy",
+	}
+
+	payload, _ := json.Marshal(info)
+	req, err := http.NewRequest("PUT", "/update-allergies", bytes.NewBuffer(payload))
+
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// invoke the target function
+	AddAllergy(rr, req)
+
+	// get the response
+	response := rr.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	// assertion
+	if http.StatusOK != response.StatusCode {
+		t.Errorf("Expected %v; got %v\n", http.StatusOK, response.StatusCode)
+	}
+
+	var resp map[string]interface{}
+	if err := json.Unmarshal(body, &resp); err != nil {
+		t.Errorf("Error unmarshalling response: %s \n", err)
+	}
+
+	expectedErrMsg := "Allergy successfully added"
+	if resp["msg"] != expectedErrMsg {
+		t.Errorf("Expected %s; got %s \n", expectedErrMsg, resp["msg"])
+	}
+
+	// TODO: clean up testAllergy
 }
