3f67df59d0daa46cd95f1de584a4a78e7d43f4f0
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-02-18 22:36:00 -0500

    build test for UserInfo function

--- back-end/api/user_test.go
+++ back-end/api/user_test.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+
 	// "fmt"
 	"net/http"
 	"net/http/httptest"
+
 	// "reflect"
 	"strings"
 	"testing"
@@ -20,34 +22,34 @@ func TestSignUpEndpoint(t *testing.T) {
 	// Create a user to be added
 	user := User{
 		FirstName: "testfirstname",
-		LastName: "testlastname",
-		Email: "unit@test.com",
-		Password: "unittest",
+		LastName:  "testlastname",
+		Email:     "unit@test.com",
+		Password:  "unittest",
 	}
 
-	// Create a mock request 
-	payload, _ := json.Marshal(user);
-	req, _ := http.NewRequest("POST", "/api/signup", bytes.NewBuffer(payload));
+	// Create a mock request
+	payload, _ := json.Marshal(user)
+	req, _ := http.NewRequest("POST", "/api/signup", bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
 
 	// Create a mock request recorder
 	rr := httptest.NewRecorder()
 
 	// Send the request and recorder
-	r.ServeHTTP(rr,req)
+	r.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusCreated {
-		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated);
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated)
 	}
 
 	expected := `{"message":"User successfully created"}`
 
 	// Remove any linebreaks from the response writer body
-	body := strings.Replace(rr.Body.String(), "\n", "", -1);
-	body = strings.Replace(body, "\r", "", -1);
+	body := strings.Replace(rr.Body.String(), "\n", "", -1)
+	body = strings.Replace(body, "\r", "", -1)
 
 	if body != expected {
-		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected);
+		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected)
 	}
 }
 
@@ -59,32 +61,32 @@ func TestLoginEndpoint(t *testing.T) {
 
 	// Create a user to be added
 	creds := Credentials{
-		Email: "unit@test.com",
+		Email:    "unit@test.com",
 		Password: "unittest",
 	}
 
-	// Create a mock request 
-	payload, _ := json.Marshal(creds);
-	req, _ := http.NewRequest("POST", "/api/login", bytes.NewBuffer(payload));
+	// Create a mock request
+	payload, _ := json.Marshal(creds)
+	req, _ := http.NewRequest("POST", "/api/login", bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
 
 	// Create a mock request recorder
 	rr := httptest.NewRecorder()
 
 	// Send the request and recorder
-	r.ServeHTTP(rr,req)
+	r.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusAccepted {
-		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated);
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated)
 	}
 
 	expected := `{"message":"User successfully logged in"}`
 
 	// Remove any linebreaks from the response writer body
-	body := strings.Replace(rr.Body.String(), "\n", "", -1);
-	body = strings.Replace(body, "\r", "", -1);
+	body := strings.Replace(rr.Body.String(), "\n", "", -1)
+	body = strings.Replace(body, "\r", "", -1)
 
 	if body != expected {
-		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected);
+		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected)
 	}
 }
