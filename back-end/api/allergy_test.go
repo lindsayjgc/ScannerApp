b2ad7ab074b5d890c88ee119ac7bba716799f2f7
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-02-26 16:51:31 -0500

    manually merge info-backend-testing TestAddAllergy

--- back-end/api/allergy_test.go
+++ back-end/api/allergy_test.go
@@ -1,9 +1,61 @@
 package main
 
-// "bytes"
-// "strings"
-// "encoding/json"
-// "fmt"
-// "net/http"
-// "net/http/httptest"
-// "testing"
+import (
+	"bytes"
+	"io/ioutil"
+
+	// "strings"
+	"encoding/json"
+	// "fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAddAllergy(t *testing.T) {
+	// Initialize router and connect to DB for this test instance
+	InitialUserMigration()
+	InitialAllergyMigration()
+	InitializeRouter()
+
+	info := Allergy{
+		Email:   "js@gmail.com",
+		Allergy: "testAllergy",
+	}
+
+	payload, _ := json.Marshal(info)
+	req, err := http.NewRequest("PUT", "/update-allergies", bytes.NewBuffer(payload))
+
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// invoke the target function
+	AddAllergy(rr, req)
+
+	// get the response
+	response := rr.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	// assertion
+	if http.StatusOK != response.StatusCode {
+		t.Errorf("Expected %v; got %v\n", http.StatusOK, response.StatusCode)
+	}
+
+	var resp map[string]interface{}
+	if err := json.Unmarshal(body, &resp); err != nil {
+		t.Errorf("Error unmarshalling response: %s \n", err)
+	}
+
+	expectedErrMsg := "Allergy successfully added"
+	if resp["msg"] != expectedErrMsg {
+		t.Errorf("Expected %s; got %s \n", expectedErrMsg, resp["msg"])
+	}
+
+	// TODO: clean up testAllergy
+}
