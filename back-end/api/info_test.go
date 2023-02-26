d04507cafaddc42d8bf56a98adc34134f3434ace
sheehan-j <jordansheehan@ufl.edu>
2023-02-26 16:42:34 -0500

    Unit test for check alleriges (alleriges found)

--- back-end/api/info_test.go
+++ back-end/api/info_test.go
@@ -40,3 +40,36 @@ func TestCheckAllerigesNotFound(t *testing.T) {
 		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected);
 	}
 }
+
+// User "unit@test.com" has allergies milk, eggs, and soy stored in the database
+func TestCheckAllerigesFound(t *testing.T) {
+	// Initialize router and connect to DB for this test instance
+	InitialUserMigration()
+	InitialAllergyMigration()
+	InitializeRouter()
+
+	ingredients := RawProductIngredients {
+		Ingredients: "milk,eggs,soy",
+	}
+
+	payload, _ := json.Marshal(ingredients);
+	req, _ := http.NewRequest("POST", "/api/check-allergies", bytes.NewBuffer(payload));
+	req.Header.Set("Content-Type", "application/json")
+	createTestCookie(req, t)
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr,req)
+
+	// Process response
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated);
+	}
+
+	expected := `{"allergies":"milk,eggs,soy","allergiesPresent":"true"}`
+	body := strings.Replace(rr.Body.String(), "\n", "", -1);
+	body = strings.Replace(body, "\r", "", -1);
+
+	if body != expected {
+		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected);
+	}
+}
