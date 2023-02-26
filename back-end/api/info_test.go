5561c18eedd1610e282893ffd933f9f17648121a
sheehan-j <jordansheehan@ufl.edu>
2023-02-26 16:37:19 -0500

    Unit test for check allergies (no allergies found)

---
+++ back-end/api/info_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestCheckAllerigesNotFound(t *testing.T) {
+	// Initialize router and connect to DB for this test instance
+	InitialUserMigration()
+	InitialAllergyMigration()
+	InitializeRouter()
+
+	ingredients := RawProductIngredients {
+		Ingredients: "walnuts",
+	}
+
+	payload, _ := json.Marshal(ingredients);
+	req, _ := http.NewRequest("POST", "/api/check-allergies", bytes.NewBuffer(payload));
+	req.Header.Set("Content-Type", "application/json")
+	createTestCookie(req, t)
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr,req)
+
+	// Process response
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated);
+	}
+
+	expected := `{"allergiesPresent":"false"}`
+	body := strings.Replace(rr.Body.String(), "\n", "", -1);
+	body = strings.Replace(body, "\r", "", -1);
+
+	if body != expected {
+		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected);
+	}
+}
