4b73c68e53ab738eb24aae725c79ca2c0a61135e
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-02-26 16:36:06 -0500

    refactor and move UserInfo test

--- back-end/api/allergy_test.go
+++ back-end/api/allergy_test.go
@@ -1,63 +1,9 @@
 package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-)
-
-func TestUserInfo(t *testing.T) {
-	// Initialize router and connect to DB for this test instance
-	InitialUserMigration()
-	InitialAllergyMigration()
-	InitializeRouter()
-
-	email := Claims{
-		Email: "fl@gmail.com",
-	}
-
-	payload, _ := json.Marshal(email)
-	req, err := http.NewRequest("GET", "/userinfo", bytes.NewBuffer(payload))
-	req.Header.Set("Content-Type", "application/json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a new ResponseRecorder to record the response
-	rr := httptest.NewRecorder()
-	var allInfo AllUserInfo
-
-	// Invoke UserInfo with the given request and response recorder
-	UserInfo(rr, req)
-
-	// Check the status code is what we expect
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	// Decode json
-	err = json.NewDecoder(rr.Body).Decode(&allInfo)
-	if err != nil {
-		t.Errorf("Can't decode json response")
-	}
-
-	// Assertions about the response
-	if allInfo.FirstName != "First" {
-		t.Error("First name not as expected")
-	}
-	if allInfo.LastName != "Last" {
-		t.Error("Last name not as expected")
-	}
-	if allInfo.Email != "fl@gmail.com" {
-		t.Error("Email not as expected")
-	}
-	// if allInfo.Password != "fl" {
-	// 	t.Error("Password not as expected")
-	// }
-	if allInfo.Allergies != "NONE" {
-		t.Error("Allergies not as expected")
-	}
-}
+// "bytes"
+// "strings"
+// "encoding/json"
+// "fmt"
+// "net/http"
+// "net/http/httptest"
+// "testing"
