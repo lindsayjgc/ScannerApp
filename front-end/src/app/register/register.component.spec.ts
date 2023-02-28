906781f7ab813f599453ee3f61cfb0e3ad4b7a5c
dylant313 <dylant313@yahoo.com>
2023-02-27 21:31:10 -0500

    Refactored Cypress tests

--- front-end/src/app/register/register.component.spec.ts
+++ front-end/src/app/register/register.component.spec.ts
@@ -1,23 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RegisterComponent } from "./register.component"
 
-import { RegisterComponent } from './register.component';
-
+//TODO: add a test for signing up the account random@gmail.com with password123
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('mounts', () => {
+    cy.mount(RegisterComponent)
+  })
+})
