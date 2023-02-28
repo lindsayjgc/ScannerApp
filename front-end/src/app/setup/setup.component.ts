efc1727cf484f43bb2da78f9bfca02022a8ce795
sarahp19 <spatel18@ufl.edu>
2023-02-28 10:49:24 -0500

    Remove unused code

--- front-end/src/app/setup/setup.component.ts
+++ front-end/src/app/setup/setup.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AllergensService } from '../services/allergens.service';
 import { Router } from '@angular/router';
 
