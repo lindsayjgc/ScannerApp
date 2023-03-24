a784aafb7eea988ac7dd83452aef86177aebe36b
sheehan-j <jordansheehan@ufl.edu>
2023-03-24 18:58:26 -0400

    Send email utility function

--- back-end/api/verify_utility.go
+++ back-end/api/verify_utility.go
@@ -1,10 +1,51 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"math/rand"
+	"net/smtp"
+	"os"
 	"strconv"
 	"time"
 )
+
+func SendEmail(toEmail string, emailType string) error {
+	// Set parameters for email 
+	from := os.Getenv("MAIL")
+	pw := os.Getenv("PW")
+	to := []string{toEmail}
+	code := GenerateRandomCode()
+	var subject string
+	var body string
+
+	// Determine subject/body based on emailType
+	if emailType == "signup" {
+		subject = "Confirm Your Signup"
+		body = "This is an email from our Grocery App for CEN3031.\n\nPlease confirm your signup by entering this code:\n" + code + "\n\nThanks!\nGroup 105"
+	} else {
+		log.Fatal("invalid email type")
+	}
+
+	// Combine subject/body into formatted message
+	message := []byte("Subject: " + subject + "\r\n" +
+	"To: " + to[0] + "\r\n" +
+	"Content-Type: text/plain; charset=UTF-8\r\n\r\n" +
+	body + "\r\n")
+
+	// Authorize with CEN3031 email login
+	auth := smtp.PlainAuth("", from, pw, "smtp.gmail.com")
+	err := smtp.SendMail("smtp.gmail.com:587", auth, from, to, message)
+
+	// Handle any email errors
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("error sending " + emailType + " email")
+	}
+
+	return nil
+}
 
 func GenerateRandomCode() string {
 	// Initialize the random number generator
