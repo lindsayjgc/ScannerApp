b0914e15c0269f0176f8c6ab033485347de3e8b7
sheehan-j <jordansheehan@ufl.edu>
2023-03-27 23:29:17 -0400

    TestResetPassword unit test

--- back-end/api/verify_test.go
+++ back-end/api/verify_test.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestVerifyEmailSignup(t *testing.T) {
@@ -138,4 +140,65 @@ func TestCheckCode(t *testing.T) {
 	// Delete the user that was created for the test
 	var deletedCode Code
 	CodeDB.Where("email = ?", "unit@test.com").Unscoped().Delete(&deletedCode)
+}
+
+func TestResetPassword(t *testing.T) {
+	// Initialize router and connect to DB for this test instance
+	InitialUserMigration()
+	InitialCodeMigration()
+	InitializeRouter()
+
+	// Add user to DB to have its password changes
+	testUser := User{
+		FirstName: "unit",
+		LastName: "test",
+		Email: "unit@test.com",
+		Password: "initialpw",
+	}
+	UserDB.Save(&testUser);
+
+	resetData := ResetData{
+		Email: "unit@test.com",
+		Password: "resetpw",
+	}
+
+	payload, _ := json.Marshal(resetData)
+	req, _ := http.NewRequest("POST", "/api/reset-password", bytes.NewBuffer(payload))
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create a mock request recorder
+	rr := httptest.NewRecorder()
+
+	// Send the request and recorder
+	r.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned the wrong status: got %v, expected %v", status, http.StatusCreated)
+	}
+
+	expected := `{"message":"Password reset successfully"}`
+
+	// Remove any linebreaks from the response writer body
+	body := strings.Replace(rr.Body.String(), "\n", "", -1)
+	body = strings.Replace(body, "\r", "", -1)
+
+	if body != expected {
+		t.Errorf("Handler returned unexpected body: got %v, expected %v", rr.Body.String(), expected)
+	}
+
+	// Also checked that the change is reflected in the DB record
+	var userSearch User
+	UserDB.Where("email = ?", "unit@test.com").First(&userSearch)
+
+	bycrptErr := bcrypt.CompareHashAndPassword(
+		[]byte(userSearch.Password),
+		[]byte("resetpw"))
+
+	if bycrptErr != nil {
+		t.Errorf("Handler returned expected status and body, but password change was not reflected in DB record")
+	}
+
+	// Clean up the database
+	var deletedUser User
+	UserDB.Where("email = ?", "unit@test.com").Unscoped().Delete(&deletedUser)
 }
