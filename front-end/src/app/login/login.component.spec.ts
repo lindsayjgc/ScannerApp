ed503f40423826a279a153cafceeee7381c2d952
dylant313 <dylant313@yahoo.com>
2023-04-13 15:20:55 -0400

    commit

--- front-end/src/app/login/login.component.spec.ts
+++ front-end/src/app/login/login.component.spec.ts
@@ -11,7 +11,7 @@ describe('login.component.spec.ts', () => {
 
   it('gets error for wrong password', () => {
     cy.mount(LoginComponent)
-    cy.get('.login-container').contains('Email').parent().find('input').type('random@gmail.com')
+    cy.get('.login-container').contains('Email').parent().find('input').type('jd@gmail.com')
     cy.get('.login-container').contains('Password').parent().find('input').type('password')
     cy.get('.login-container').find('button').contains('Login').click()
     cy.get('body').find('mat-snack-bar-container').should('contain.text', 'Incorrect password')
