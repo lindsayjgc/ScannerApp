293ffcd49953468240a9c70f4fa38efa06f4eafc
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-04-14 23:47:25 -0400

    setup adding search to database

--- back-end/api/search.go
+++ back-end/api/search.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/glebarez/sqlite"
@@ -14,6 +16,10 @@ type Search struct {
 	Email string `json:"email"`
 	Query string `json:"query"`
 	Count int    `json:"count"`
+}
+
+type RawSearch struct {
+	Query string `json:"query"`
 }
 
 var SearchDB *gorm.DB
@@ -40,4 +46,42 @@ func InitialSearchMigration() {
 	jwtKey = []byte(os.Getenv("SECRET_KEY"))
 
 	SearchDB.AutoMigrate(&Search{})
+}
+
+func SaveQuery(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Check for logged in user and get their email
+	claims, err, resStatus := CheckCookie(w, r)
+
+	if err != nil {
+		w.WriteHeader(resStatus)
+		json.NewEncoder(w).Encode(GenerateResponse(err.Error()))
+		return
+	}
+
+	var query RawSearch
+	err = json.NewDecoder(r.Body).Decode(&query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(GenerateResponse("Error decoding JSON"))
+		return
+	}
+
+	var existingQuery Search
+	res := ListDB.First(&existingQuery, "email = ? AND query = ?", claims.Email, query.Query)
+
+	if res.Error == nil {
+		existingQuery.Count = existingQuery.Count + 1
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(GenerateResponse("Query count updated"))
+		return
+	}
+
+	// TODO: check if there is a response from the query
+	search := Search{Email: claims.Email, Query: query.Query, Count: 1}
+	SearchDB.Create(&search)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(GenerateResponse("Query count updated"))
 }
