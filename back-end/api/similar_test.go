7119f45e3034469dc500dcb96330b417d0b2669f
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-04-17 19:44:33 -0400

    write tests for getting food list

--- back-end/api/similar_test.go
+++ back-end/api/similar_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestFetchFoodListPage(t *testing.T) {
+	// Ensure that GetFoodList returns a non-empty list
+	foodList, err := fetchFoodListPage(1)
+	if err != nil {
+		t.Errorf("Error fetching food list: %v", err)
+	}
+	if len(foodList) == 0 {
+		t.Errorf("Food list is empty")
+	}
+}
+
+func TestGetFoodList(t *testing.T) {
+	// Ensure that GetFoodList returns a non-empty list
+	foodList, err := GetFoodList()
+	if err != nil {
+		t.Errorf("Error fetching food list: %v", err)
+	}
+	if len(foodList) == 0 {
+		t.Errorf("Food list is empty")
+	}
+}
