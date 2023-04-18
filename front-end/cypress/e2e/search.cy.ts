86f5e58bf070b482533fd432ed026733c44b6dd7
dylant313 <dylant313@yahoo.com>
2023-04-18 14:56:01 -0400

    Update search test

--- front-end/cypress/e2e/search.cy.ts
+++ front-end/cypress/e2e/search.cy.ts
@@ -29,4 +29,16 @@ describe('search functionality', () => {
     cy.get('button').contains('New search').click()
     cy.url().should('include', '/home')
   })
+
+  it('searches for a specific product', () => {
+    //Enter barcode number
+    cy.visit('http://localhost:4200/home')
+    cy.contains('Category').click()
+    cy.contains('Barcode').click()
+    cy.get('mat-form-field').contains('Enter search term').click().type('8410000001013')
+    cy.get('button[type="submit"]').click()
+
+    //Should go directly to product page
+    cy.get('div[product-name]').should('contain.text', 'Chips Ahoy Original')
+  })
 })
