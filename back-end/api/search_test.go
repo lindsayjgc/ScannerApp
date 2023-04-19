99c6aad73bf85179e4f0bbf11f9b961d64fe88a1
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-04-19 11:18:17 -0400

    Write test for GetQueries function

--- back-end/api/search_test.go
+++ back-end/api/search_test.go
@@ -42,3 +42,44 @@ func TestSaveQuery(t *testing.T) {
 	query := Search{Email: "unit@test.com", Query: "testQuery", Count: 1}
 	SearchDB.Where("email = ? AND query = ?", query.Email, query.Query).Unscoped().Delete(&query)
 }
+
+func TestGetQueries(t *testing.T) {
+	InitialUserMigration()
+	InitialSearchMigration()
+	InitializeRouter()
+
+	query1 := Search{Email: "unit@test.com", Query: "testQuery1", Count: 1}
+	query2 := Search{Email: "unit@test.com", Query: "testQuery2", Count: 2}
+	SearchDB.Create(&query1)
+	SearchDB.Create(&query2)
+
+	req, _ := http.NewRequest("GET", "search", nil)
+	rr := httptest.NewRecorder()
+	req.Header.Set("Content-Type", "application/json")
+
+	createTestCookie(req, t)
+
+	GetQueries(rr, req)
+
+	response := rr.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	if response.StatusCode != http.StatusOK {
+		t.Errorf("Error: Expected %v, got %v", http.StatusOK, response.StatusCode)
+	}
+
+	var resList []Query
+	json.Unmarshal(body, &resList)
+
+	expected := []string{"testQuery1", "testQuery2"}
+	if resList[0].Query != "testQuery1" {
+		t.Errorf("Expected %s; got %s \n", expected[0], resList[0].Query)
+	}
+	if resList[1].Query != "testQuery2" {
+		t.Errorf("Expected %s; got %s \n", expected[1], resList[1].Query)
+	}
+
+	SearchDB.Where("email = ? AND query = ?", query1.Email, query1.Query).Delete(&query1)
+	SearchDB.Where("email = ? AND query = ?", query2.Email, query2.Query).Delete(&query2)
+
+}
