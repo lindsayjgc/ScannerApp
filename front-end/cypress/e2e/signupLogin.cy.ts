928c5adc684e1c063b2ce075dcf11852ae5a1173
dylant313 <dylant313@yahoo.com>
2023-04-19 13:34:29 -0400

    Added search recording, added more login checks

--- front-end/cypress/e2e/signupLogin.cy.ts
+++ front-end/cypress/e2e/signupLogin.cy.ts
@@ -59,6 +59,12 @@ describe("Add User", () => {
     cy.get('.login-container').find('button').contains('Login').click()
     cy.url().should('include', '/home')
 
+    //Get redirected to home when already logged in
+    cy.visit('http://localhost:4200/login')
+    cy.url().should('include', '/home')
+    cy.visit('http://localhost:4200/register')
+    cy.url().should('include', '/home')
+
     //Confirm that logging out goes to log in page
     cy.get('.toolbarContainer').find('button').click()
     cy.contains('Log out').click()
