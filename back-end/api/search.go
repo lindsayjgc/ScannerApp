c317d05e5c4da48e24af6ddc2838cb9acd289c53
haddie7979 <88420589+haddie7979@users.noreply.github.com>
2023-04-19 17:30:06 -0400

    fix search functions

--- back-end/api/search.go
+++ back-end/api/search.go
@@ -83,7 +83,7 @@ func SaveQuery(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: check if there is a response from the query
 	search := Search{Email: claims.Email, Query: query.Query, Count: 1}
-	SearchDB.Create(search)
+	SearchDB.Create(&search)
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(GenerateResponse("Query count updated"))
